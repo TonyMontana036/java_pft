0d7b27aaf26ffd08285dfcd8304d084fd5581216
TonyMontana036 <mudaq@mail,ru>
2019-02-18 21:51:37 +0300

    if then else 3 теста
    Формула квадратного уравнения

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/EquationTests.java
@@ -0,0 +1,26 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.sandbox.Equation;
+
+public class EquationTests {
+
+  @Test
+  public void test0 (){
+    Equation e = new Equation(1, 1, 1);
+    Assert.assertEquals(e.rootNubre(),0);
+  }
+
+  @Test
+  public void test1 (){
+    Equation e = new Equation(1, 2, 1);
+    Assert.assertEquals(e.rootNubre(),1);
+  }
+
+  @Test
+  public void test2 (){
+    Equation e = new Equation(1, 5, 6);
+    Assert.assertEquals(e.rootNubre(),2);
+  }
+}
