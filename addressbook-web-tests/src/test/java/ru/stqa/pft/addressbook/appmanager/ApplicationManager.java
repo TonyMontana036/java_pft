3cb2b53322b7ef5f00690e843d91b3f78bd2d6cc
TonyMontana036 <mudaq@mail,ru>
2019-02-25 22:00:42 +0300

    Коллекции
    Проверка кол-ва групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -34,7 +34,7 @@ public class ApplicationManager {
     }
 
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    driver.get("http://localhost/addressbook/group.php");
+    driver.get("http://localhost/addressbook");
     groupHelper = new GroupHelper(driver);
     navigationHelper = new NavigationHelper(driver);
     sessionHelper = new SessionHelper(driver);
