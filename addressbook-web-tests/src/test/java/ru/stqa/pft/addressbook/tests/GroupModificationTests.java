561aefc504745e13eade1a42cf42013a4061998b
TonyMontana036 <mudaq@mail,ru>
2019-03-04 21:52:28 +0300

    выбор элемента из списка

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -10,7 +10,7 @@ public class GroupModificationTests extends TestBase {
   public void testGroupModification(){
     app.getNavigationHelper().gotoGroupPage();
     int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("123", "456", "789"));
     app.getGroupHelper().submitGroupModification();
